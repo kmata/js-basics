ed1eca45c06fdf8d8cc2db43291326c3428126da
Build <karla-build@Karlas-Air.fritz.box>
2021-07-25 22:12:08 +0200

    Introduction to JS

---
+++ index.js
@@ -0,0 +1,40 @@
+ //First Javascript code
+ console.log('Hello World');
+
+// let name = 'Karla'; 
+ //console.log(name);
+
+// Rules for variables
+// Cannot be a reserved keyword 
+// Should be meaningful 
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+
+//Constans 
+const interestRate = 0.3;
+console.log (interestRate);
+
+
+// Values types 
+//let name = 'Karla'; //String
+//let age = 31; //Number Literal
+let isApproved = true; //Boolean Literal 
+let firstName = undefined;
+let lastName = null;
+
+//Objects 
+let person = {
+    name:'Karla',
+    age: 31
+}
+
+//Dot Notation - Change the value 
+person.name = 'Vanne';
+
+//Bracket Notation
+person['name'] = 'Fede';
+
+console.log(person);
+console.log(person.name);
